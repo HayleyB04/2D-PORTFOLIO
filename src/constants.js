a0bc894ef946a3902cf167a560a8b8a6dac7d368
HayleyB04 <hayleylford@gmail.com>
2024-08-02 20:07:28 +0100

    first commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    key: `In the future I want to expand my GitHub portfolio to showcase the skills I learn. Follow me if you would like to see more updates!.`,
+    bunny: `Welcome to my virtual portfolio. Please explore to find out more about me and what I have been doing`,
+    bookshelf_1: `I am currently studying the Software and Data Engineering pathway with CodeFirstGirls.`,
+    bookshelf_2: `In additional to my technical qualifications, I have a BSc in Pychology and a PGCE in Primary Education.`,
+    pc: `My technical skills include Python, MySQL, Git, GitHub and JavaScript.`,
+    bedside_table: `In my spare time I am listening to podcasts in order to stay up to date with Tech news. One of my current favourites is 'Women Tech Charge'.`,
+    tv: `When not studying, I enjoy playing video games, travelling and watching a variety of sports.`,
+    desk_table: `I gained a lot of important skills from my previous career as a teacher. It also allowed me to live in different countries around the world.`,
+    table: `I am interested in Artificial Intelligence and have been carrying out some freelance work training AI algorithms in order to learn more.`,
+};
+
